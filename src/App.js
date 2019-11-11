a7c3d8586031b8987eea920c50d46bfd90dfd90f
Jovan Derebanoski <jd7@students.kiron.ngo>
2019-11-11 01:18:43 +0100

    BeachResort : Navbar Finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import Home from './Components/Home';
-import Rooms from './Components/Rooms';
-import SingleRoom from './Components/SingleRoom';
-import Error from './Components/Error';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
+
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <>
+    <Navbar />
+    <Switch>
       <Route exact path="/"            component={Home} />
       <Route exact path="/rooms"       component={Rooms} />
       <Route exact path="/rooms/:slug" component={SingleRoom} />
-  
-
+      <Route                           component={Error} />
+  </Switch>
       
     </>
 
