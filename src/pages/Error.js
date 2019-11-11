0a3e3f281e76a74562de8f9a5f62ca6013351eae
Jovan Derebanoski <jd7@students.kiron.ngo>
2019-11-11 01:47:37 +0100

    BeachResort : Hero IMG Component Finished

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 function Error() {
-    return (
-        <div>
-            Hello from Error Page
-        </div>
-    )
+    return <Hero>
+        <Banner title="404" subtitle="page not found">
+            <Link to='/' className="btn-primary">
+                Return Home
+            </Link>
+        </Banner>
+    </Hero>
 }
 
 export default Error
